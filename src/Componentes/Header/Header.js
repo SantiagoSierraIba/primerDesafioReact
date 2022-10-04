8ab58f481b48c034eb1f5be779b5ab9a396b114c
Santiago Sierra <santiagosierraiba@gmail.com>
2022-10-03 22:50:46 -0500

    AñadiendoComponentes

---
+++ src/Componentes/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+
+export const Header = () => {
+  return (
+    <header>
+        <img src="" alt="" />
+        <h1>Wuacamoli</h1>
+        <nav>
+            <a href="">Categoría 1</a>
+            <a href="">Categoría 2</a>
+            <a href="">Categoría 3</a>
+            <a href="">Categoría 4</a>
+        </nav>
+        <LocalMallIcon />
+    </header>
+  )
+}
+
+export default Header
